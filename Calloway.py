9393bacea892df0eebcb7e834d074e0ab3a4cb60
Brett Silvey <brett@silveys.net>
2023-08-09 16:03:12 -0700

    User Input & Scorecard Generation
    Generate Scorecard using User Input. Computer the front 9, back 9 and total score. Sort the Scorecard list in reverse so that the worst holes are first. This will make the Handicap deduction easier later on I think.

--- Calloway.py
+++ Calloway.py
@@ -1,1 +1,32 @@
-print("HelloWorld")
+import os
+import numpy
+
+scorecard = [4, 4, 9, 3, 8, 5, 7, 5, 5, 3, 5, 3, 5, 7, 4, 5, 5, 5]
+# scorecard = []
+
+#Ask for the scores for each hole and append to scorecard ---  THIS WORKS
+# hole = 1
+# while hole <= 18:
+#     userInput = int(input("Enter Score for Hole " + str(hole) + ": "))
+#     scorecard.append(userInput)
+#     hole += 1
+
+front9 = 0
+back9 = 0
+
+# Find the Total for front 9 holes
+for x in range(0, 9):
+    front9 += scorecard[x]
+# Find the Total for back 9 holes
+for x in range(9, 18):
+    back9 += scorecard[x]
+
+# Print the Front, Back and Total Scores
+print("Out: " + str(front9))
+print("In: " + str(back9))
+print("Total: " + str(front9+back9))
+
+scorecard.sort(reverse=True)
+
+for x in range(len(scorecard)):
+    print(scorecard[x])
