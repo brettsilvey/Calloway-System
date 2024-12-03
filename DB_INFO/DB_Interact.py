c4ca6764d3c2de615f75291398272edf66dae949
Brett Silvey <brett@silveys.net>
2024-12-02 20:15:26 -0800

    DB_Interact - Updated
    Removed dead code, cleaned up insert/fetch functions.

--- DB_INFO/DB_Interact.py
+++ DB_INFO/DB_Interact.py
@@ -10,44 +10,79 @@ calloway_scoring database. The following functions are available:
 
 
 # Delete a row from the team_info table
-def delete_row(teamName):
+def delete_Team(teamName):
     connection = connect_to_database()
-    if connection:
-        try:
-            cursor = connection.cursor()
-            cursor.execute(f"DELETE FROM team_info WHERE team_name = '{teamName}'")
-            connection.commit()
-            print("Row deleted successfully")
-        except mysql.connector.Error as err: # check for errors
-            print(f"Error: {err}")
-        finally:
-            cursor.close() # delete the cursor
-            connection.close() # close the connection
+    try:
+        cursor = connection.cursor()
+        queryteamID = f"Select from teams where teamName = '{teamName}'"
+        cursor.execute(queryteamID)
+        teamID = cursor.fetchone()
+        cursor.execute(f"DELETE FROM teams WHERE team_name = '{teamID}'")
+        connection.commit()
+        print("Row deleted successfully")
+    except mysql.connector.Error as err: # check for errors
+        print(f"Error: {err}")
+    finally:
+        cursor.close() # delete the cursor
 
 # Insert a new row into the table given a team name from the user
 def insert_TeamName(teamName):
     connection = connect_to_database() # connect to the database
-    if connection: # verify connection
-        try:
-            cursor = connection.cursor() # create cursor object (Access R/W to DB)
-            query = f"INSERT INTO team_info (team_name) VALUES ('{teamName}')" # SQL query to insert a new teamName Row
-            cursor.execute(query) # execute the SQL query
-            connection.commit() # commit the changes to the database
-            print(teamName + " created successfully") # print success message
-        except mysql.connector.Error as err:
-            print(f"Error: {err}")
-        finally:
-            cursor.close() # close the curosor
-            connection.close() # close the connection
+    try:
+        cursor = connection.cursor() # create cursor object (Access R/W to DB)
+        query = f"INSERT INTO team_info (team_name) VALUES ('{teamName}')" # SQL query to insert a new teamName Row
+        cursor.execute(query) # execute the SQL query
+        connection.commit() # commit the changes to the database
+        print(teamName + " created successfully") # print success message
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close() # close the curosor
 
 # Fetch the team information from the table
 def fetch_TeamName(teamName):
     try:
         connection = connect_to_database()
-        if connection:
-            cursor = connection.cursor()
-            query = f"SELECT * FROM team_info WHERE team_name = '{teamName}'"
-            cursor.execute(query)
+        cursor = connection.cursor()
+        query = f"SELECT * FROM team_info WHERE team_name = '{teamName}'"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except mysql.connector.Error as err: # check for errors
+        print(f"Error: {err}")
+    finally:
+        cursor.close() # delete the cursor
+
+# Insert new scorecard
+def insert_Scorecard(teamName, courseName):
+    connection = connect_to_database() #connect to the database
+    try:
+        cursor = connection.cursor() # create cursor object
+        queryTeamID = f"SELECT teamID from teams where teamName = '{teamName}'"
+        cursor.execute(queryTeamID) # query TeamID using TeamName
+        teamID = cursor.fetchone()
+        if teamID: # if teamID exists
+            queryInsert = f"Insert INTO scorecards('{teamID}', '{courseName}')" # insert teamID and CourseName into scorecard
+            cursor.execute(queryInsert) # execute the query
+            connection.commit() # commit the query to the database
+            print("Scorecard created successfully") # print successful message
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close() # close the cursor
+
+# Fetch Information from Scorecard
+def fetch_Scorecard(teamName):
+    try:
+        connection = connect_to_database() # connect to the database - this can all be removed and placed in the maine function
+        cursor = connection.cursor()
+        queryTeamName = f"SELECT teamID from teams where teamName = '{teamName}'"
+        cursor.execute(queryTeamName)
+        teamID = cursor.fetchone()
+        if teamID:
+            insertQuery = f"SELECT * FROM scorecard WHERE teamID = '{teamID}'"
+            cursor.execute(insertQuery)
             rows = cursor.fetchall()
             for row in rows:
                 print(row)
@@ -55,4 +90,3 @@ def fetch_TeamName(teamName):
         print(f"Error: {err}")
     finally:
         cursor.close() # delete the cursor
-        connection.close() # close the connection
--- DB_INFO/DB_test.py
+++ DB_INFO/DB_test.py
@@ -5,6 +5,10 @@ from DB_Interact import delete_row, insert_TeamName, fetch_TeamName
 def main():
     #user_input = input("Please enter a team name: ")
     #print(user_input)
-    connect_to_database()
+    connection = connect_to_database()
+    if connection:
+        # do database stuff
+        print("Connected to the database")
+    connection.close()
 
 main()
