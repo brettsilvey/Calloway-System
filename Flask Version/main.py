51648882c1df58528cba0056db12edf2249ae937
Brett Silvey <brett@silveys.net>
2024-12-03 17:58:34 -0800

    Base Flask Concept Complete
    Created templates folder, and base.html to be used in additional pages.

--- Flask Version/main.py
+++ Flask Version/main.py
@@ -1,36 +1,29 @@
 from flask import Flask, redirect, url_for, render_template
 from flask_mysqldb import MySQL
 from DB_Connect import connect_to_database
-from DB_Interact import delete_Team, insert_TeamName, fetch_TeamNameInfo, insert_Scorecard, fetch_ScorecardID, insert_scores, fetch_scores
+from DB_Interact import delete_Team, fetch_TeamNameInfo, insert_Scorecard, fetch_ScorecardID, insert_scores, fetch_scores
 from create_Objects import create_ScorecardOBJs
 from classes import Scorecard_OBJ
 
 app = Flask(__name__)
 
-# MySQL configurations
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '12345'
-app.config['MYSQL_DB'] = 'calloway_scoring'
-
-mysql = MySQL(app)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "Hello"
+    return render_template("home.html")
 
 @app.route("/leaderboards")
 def leaderboards():
-    return "This is the leaderboards page."
+    return render_template("leaderboards.html")
 
-@app.route("/Scorecard")
+@app.route("/createScorecards")
 def createScorecards():
-    return "Create new Scorecards here"
+    return render_template("createScorecards.html")
 
 @app.route("/db_test")
 def db_test():
-    return str(fetch_TeamNameInfo('tester'))
+    return str(fetch_TeamNameInfo('brettsTester'))
 
 if __name__ == "__main__":
     app.run()
